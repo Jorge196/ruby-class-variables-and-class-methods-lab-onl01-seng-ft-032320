6f61af53486e071684b849f02c151d0a47e94b9a
Jorge Ramirez <joramirez96@gmail.com>
2020-04-03 17:02:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,15 @@
 class Song
   attr_accessor :artists, :genres, :name
 
+  def initialize(artists)
+    @artists = artists
+  end
+
   def artists=(artists)
     @artists = artists
   end
 
   def artists
     @artists
-  end 
+  end
 end
